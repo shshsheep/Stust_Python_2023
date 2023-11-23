234cee6fc3e23f5002e0f3af9bab702dfa61b477
shshsheep <tung.lung38@gmail.com>
2023-11-23 11:48:54 +0800

    #annotation
    change annotation in english

--- exercise_1123/test.py
+++ exercise_1123/test.py
@@ -1,13 +1,14 @@
 class MyShape:
     def __init__(self,name ,id, salary ,department):
+        #初始化，建立新員工時被呼叫
         self.id = id 
         self.name = name 
         self.salary = salary 
         self.departmeant = department 
     
     def calculate_salary(self,hour_works): #計算超時工作
-        if hour_works > 50 :
-            overtime = hour_works - 50
+        if hour_works > 50 : #超過50小時
+            overtime = hour_works - 50 
             overtime_amount = (overtime*(self.salary/50))
             total_salary = self.salary + overtime_amount
             return total_salary
@@ -23,11 +24,13 @@ class MyShape:
         print("Employee 's salary :", self.salary)
         print("Employee 's department :", self.departmeant)
 
+#Ex Employee 's details
 sample_id1 = MyShape("ADAMS", "E7876", 50000, "ACCOUNTING")
 sample_id2 = MyShape("JONES", "E7499", 45000, "RESEARCH")
 sample_id3 = MyShape("MARTIN", "E7900", 50000, "SALES")
 sample_id4 = MyShape("SMITH", "E7698", 55000, "OPERATIONS")
 
+#Using sample ex
 sample_id1.assign_department("Chef")
 
 sample_id1.print_employee_details()
