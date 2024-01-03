aa615e8cd56680aac6b4bba7c66bdd8175417c44
shshsheep <tung.lung38@gmail.com>
2024-01-04 00:54:39 +0800

    add where and borrowtime

--- practise_0103/test02.py
+++ practise_0103/test02.py
@@ -1,23 +1,25 @@
 class Library :   
 
     class Books :
-        def __init__(self,id,bookname,year,writer) :
+        def __init__(self,id,bookname,year,writer,where) :
             self.bookname = bookname
             self.id = id 
             self.year = year
             self.writer = writer
+            self.location = where
 
 
     class User:
-        def __init__(self,name,id):
+        def __init__(self,name,User_id,time):
             self.Username = name 
-            self.Userid = id 
+            self.Userid = User_id 
+            self.time = time
             self.books = {}
             
         def add_book(self, book) : 
             if book.id not in self.books:
                 self.books[book.id] = book
-                print(f"{self.Username} 借閱 : {book.bookname} ID : {book.id} 年份 : {book.year} 作者 : {book.writer}")
+                print(f"{self.Username} 借閱 : {book.bookname} ID : {book.id} 年份 : {book.year} 作者 : {book.writer} 藏書位置 : {book.location}")
             else :
                 print(f"{book.bookname} 已經被借出")
 
@@ -35,15 +37,15 @@ class Library :
             else :
                 print(f"{self.Username} 目前借閱的書 :")
                 for book_id , book in self.books.items() : 
-                    print(f"書籍 ID ： {book_id}, 書名：{book.bookname} 作者：{book.writer} 年份：{book.year}")
+                    print(f"書籍 ID ： {book_id} 書名：{book.bookname} 作者：{book.writer} 年份：{book.year} 借閱時間 : {self.time}")
 
 #ex
 library = Library()
 
-book1 = library.Books("001","A1","2024","andy")
-book2 = library.Books("002","S2","2022","mike")
+book1 = library.Books("001","A1","2024","andy","2F-1")
+book2 = library.Books("002","S2","2022","mike","2F-46")
 
-user1 = library.User("Jhon","022")
+user1 = library.User("Jhon","022","2024-01-03")
 
 
 
